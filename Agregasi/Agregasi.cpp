421032825a0781c2654e297c0be011c07ce74c9a
M S I <M S I@MSI>
2024-05-20 11:35:41 +0700

    ifndef ibu

--- Agregasi/Agregasi.cpp
+++ Agregasi/Agregasi.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 #include "Anak.h"
 #include "Ibu.h"
+
+int main() {
+	ibu* varIbu = new ibu("dini");
+	ibu* varIbu2 = new ibu("novi");
+	anak* varAnak1 = new anak("tono");
+	anak* varAnak2 = new anak("rini");
+	anak* varAnak3 = new anak("dewi");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+}
