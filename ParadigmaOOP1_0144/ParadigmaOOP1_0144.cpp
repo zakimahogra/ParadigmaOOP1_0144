2f4a65136d61de059afbdba927276a72b02ab3cf
M S I <M S I@MSI>
2024-05-20 10:37:27 +0700

    menampilkan id,nama,nilai mahasiswa

--- ParadigmaOOP1_0144/ParadigmaOOP1_0144.cpp
+++ ParadigmaOOP1_0144/ParadigmaOOP1_0144.cpp
@@ -9,6 +9,13 @@ private:
 	float nilai;
 
 public:
-	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama)
-}
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama) {
+	}
+
+	~mahasiswa() {
+		cout << "Id   = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
+};
 
