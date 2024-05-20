a876b43c412b7e3bdf8e7ad58db0b1f2a19e9b21
M S I <M S I@MSI>
2024-05-20 11:36:35 +0700

    agregasi

--- Agregasi/Agregasi.cpp
+++ Agregasi/Agregasi.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
 	ibu* varIbu = new ibu("dini");
 	ibu* varIbu2 = new ibu("novi");
-	anak* varAnak1 = new anak("tono");
+	anak* varAnak1 = new anak("tomo");
 	anak* varAnak2 = new anak("rini");
 	anak* varAnak3 = new anak("dewi");
 
